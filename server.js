be33ece2df5a8fff004df1d3a494b1ca34acb416
Benjamin Krier <benkrier@gmail.com>
2016-09-29 18:17:48 -0500

    Add UPDATE and DELETE functions

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ MongoClient.connect('mongodb://obiwan:04b13a77k@ds045465.mlab.com:45465/express-
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   db.collection('quotes').find().toArray((err, result) => {
@@ -31,5 +33,29 @@ app.post('/quotes', (req, res) => {
 
     console.log('saved to database')
     res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsort: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) =>{
+  db.collection('quotes').findOneAndDelete({name: req.body.name},
+  (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A Darth Vader quote was deleted.')
   })
 })
