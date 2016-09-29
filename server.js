f99efcbd8c115ca1f265b18aae0786d673c1c07e
Benjamin Krier <benkrier@gmail.com>
2016-09-29 13:40:45 -0500

    Initialize repo and add quotepost app

---
+++ server.js
@@ -0,0 +1,35 @@
+console.log('May Node be with you')
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://obiwan:04b13a77k@ds045465.mlab.com:45465/express-quoteposts', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, results) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
